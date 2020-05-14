3a20b7fb21f41187bd9062cbca4f6111e827f5db
Damilare Shobowale <oluwadamilareshobowale@gmail.com>
2020-05-14 01:54:20 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Plusity Blogs Frontend
---
+++ pages/index.js
@@ -0,0 +1,6 @@
+export default () => (
+  <div className="p-4 shadow rounded bg-white">
+    <h1 className="text-purple-500 leading-normal">Plusity Blog</h1>
+    <p className="text-gray-500">Coming Soon</p>
+  </div>
+)
